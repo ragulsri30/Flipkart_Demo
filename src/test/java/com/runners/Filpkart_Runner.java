d8b97fbd1650396709363384e47ab0d3e9cebcdd
ragulsri30 <109237608+ragulsri30@users.noreply.github.com>
2024-06-20 15:03:10 +0530

    Update Filpkart_Runner.java

--- src/test/java/com/runners/Filpkart_Runner.java
+++ src/test/java/com/runners/Filpkart_Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Flipkar_Features_File",glue = {"com.stepdefinition","com.hooks.Hooks"}, tags = "@Regrassion")
+@CucumberOptions(features = "src/test/resources/Flipkar_Features_File",glue = {"com.stepdefinition","com.hooks.Hooks"}, tags = "@new")
 public class Filpkart_Runner {
 
 }
